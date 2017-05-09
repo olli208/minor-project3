e6716e1b82526980b9004c4f54f16c41d9ace28d
Oliver <hamburger602@gmail.com>
2017-05-09 15:24:58 +0200

    added packages for several things

--- public/js/script.js
+++ public/js/script.js
@@ -1,28 +1,9 @@
-function myMove() {
-    var elem = document.querySelector(".arrow");
-    var pos = 0;
-    var id = setInterval(frame, 3);
-    function frame() {
-        if (pos == 600) {
-            clearInterval(id);
-        } else {
-            pos++;
-            elem.style.top = pos + 'px';
-            elem.style.left = pos + 'px';
-        }
-    }
-}
+// ANDERE CODE
+/* Get all div elements. */
+var leftArrow = document.querySelectorAll(".arrow_left");
+var upArrow = document.querySelectorAll(".arrow_left");
+var downArrow = document.querySelectorAll(".arrow_left");
+var rightArrow = document.querySelectorAll(".arrow_left");
 
-myMove();
-
-// andere code niks met deze te maken
-var start = Date.now();
-
-var timer = setInterval(function() {
-    var timePassed = Date.now() - start;
-
-    // train.style.left = timePassed / 5 + 'px';
-
-    if (timePassed > 2000) clearInterval(timer);
-
-}, 20);
+/* Animate all divs at once. */
+Velocity(leftArrow, { translateY: '80vh' }, 3000);
--- server.js
+++ server.js
@@ -4,6 +4,12 @@ var path = require('path');
 var io = require('socket.io');
 var http = require('http');
 var request = require('request');
+var compression  = require('compression');
+
+app.use(compression({
+        threshold: 0,
+        filter: () => true,// Compress all assets by default
+}));
 
 // socket.io
 http = http.createServer(app);
