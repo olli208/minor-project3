cc1e6f3bdd78eac7e22b88ce84a4347484e0c4f1
Oliver <hamburger602@gmail.com>
2017-05-10 11:48:39 +0200

    animating progressbar

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,22 @@
-// ANDERE CODE
-/* Get all div elements. */
-var leftArrow = document.querySelectorAll(".arrow_left");
+var leftArrow = document.querySelectorAll(".arrow-left");
 var upArrow = document.querySelectorAll(".arrow_left");
 var downArrow = document.querySelectorAll(".arrow_left");
 var rightArrow = document.querySelectorAll(".arrow_left");
 
 /* Animate all divs at once. */
-Velocity(leftArrow, { translateY: '80vh' }, 3000);
+Velocity(
+  leftArrow, {
+    translateY: '80vh'
+  }, 3000);
+
+
+document.querySelectorAll('#outer').forEach(function(e) {
+  e.addEventListener('click' , myfunction);
+});
+
+function myfunction(e){
+  Velocity(e.target.childNodes[3], {
+    height: '100%'
+  }, 1500);
+}
+
