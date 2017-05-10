b3f2d250e48cf5ad551f13ecf79e083f497c4c43
Oliver <oliveroenang@gmail.com>
2017-05-10 20:09:21 +0200

    why so many whitespace!

--- public/js/script.js
+++ public/js/script.js
@@ -81,7 +81,6 @@ function Arrow(direction) {
 
 // To enable animating the arrows
 Arrow.prototype.step = function() {
-
   // Controls the speed of the arrows
   this.image.css("top", "+=4px");
 
@@ -89,10 +88,8 @@ Arrow.prototype.step = function() {
 
 // Deletes arrows when they get to bottom of page
 Arrow.prototype.destroy = function() {
-
   // removes the image of the DOM
   this.image.remove();
-
   // Removes the note/arrow from memory/array
   notes.splice(0,1);
 
@@ -100,12 +97,8 @@ Arrow.prototype.destroy = function() {
 
 // Explodes arrow when hit
 Arrow.prototype.explode = function() {
-
   this.image.remove();
-
 };
-
-
 
 // For random arrows
 var randNum = 0;
@@ -116,82 +109,53 @@ var frame = 0;
 // Determines the speed of notes
 var arrowSpawnRate = 40;
 
-
 // Random generator for arrows
 function randomGen() {
-
   // Randomizes between 1 and 4
   randNum = Math.floor(Math.random() * 4) + 1;
-
   if (randNum === 1) {
-
     notes.push(new Arrow("left"));
-
   }
   if (randNum === 2) {
-
     notes.push(new Arrow("right"));
-
   }
   if (randNum === 3) {
-
     notes.push(new Arrow("up"));
-
   }
   if (randNum === 4) {
-
     notes.push(new Arrow("down"));
-
   }
-
 }// ends randomGen()
-
 
 // Render function //
 function render() {
-
   if (frame++ % arrowSpawnRate === 0) {
-
     randomGen();
-
   }
 
   // Animate arrows showering down //
   for (var i = notes.length - 1; i >= 0; i--) {
-
     notes[i].step();
-
     // Check for cleanup
     if (notes[i].image.position().top > 615) {
-
       notes[i].destroy();
-
     }
-
   }
 
 }// ends render()
 
-
-
 // jQuery to animate arrows //
 $(document).click(function () {
-
   // shim layer with setTimeout fallback
   window.requestAnimFrame = (function() {
-
     return window.requestAnimationFrame ||
-
     window.webkitRequestAnimationFrame ||
-
     window.mozRequestAnimationFrame ||
-
     function(callback) {
 
       window.setTimeout(callback, 40 / 75);
 
     };
-
   })();
 
   /*  place the rAF *before* the render()
@@ -200,60 +164,36 @@ $(document).click(function () {
 
   // Infinte loop for game play
   (function animloop() {
-
     requestAnimFrame(animloop);
-
     render();
-
   })();// ends (function animloop() )
-
-
 });// ends $(doc).ready
-
 
 
 // Listening for when the key is pressed
 $(document).keydown( function(event) {
-
   for (var i = 0; i < notes.length; i++) {
-
       console.log(notes[i].image.position().top);
 
     if (event.keyCode == 37 && notes[i].direction == "left") {
-
       if (notes[i].image.position().top > 490 && notes[i].image.position().top < 530) {
-
         console.log("LEFT! "+notes[i].explode());
-
       }
-
     }
     if (event.keyCode == 38 && notes[i].direction == "up") {
-
       if (notes[i].image.position().top > 490 && notes[i].image.position().top < 530) {
-
         console.log("UP! "+notes[i].explode());
-
       }
-
     }
     if (event.keyCode == 40 && notes[i].direction == "down") {
-
       if (notes[i].image.position().top > 490 && notes[i].image.position().top < 530) {
-
         console.log("DOWN! "+notes[i].explode());
-
       }
-
     }
     if (event.keyCode == 39 && notes[i].direction == "right") {
-
       if (notes[i].image.position().top > 490 && notes[i].image.position().top < 530) {
-
         console.log("RIGHT! "+notes[i].explode());
-
       }
-
     }
 
   }// ends loop
